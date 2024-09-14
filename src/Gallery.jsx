8848f5bacfbababff4d80b3426614b12b6022f61
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-14 18:17:58 +0500

    Initial commit

---
+++ src/Gallery.jsx
@@ -0,0 +1,23 @@
+import LightGallery from 'lightgallery/react';
+
+/
+
+  function Gallery() {
+    const images = [
+        {src: "/1.jpg", alt: "1"},
+        {src: "/2.jpg", alt: "2"},
+        {src: "/3.jpg", alt: "3"},
+        {src: "/4.jpg", alt: "4"},
+        {src: "/5.jpg", alt: "5"},
+        {src: "/6.jpg", alt: "6"},
+        {src: "/7.jpg", alt: "7"},
+        {src: "/8.jpg", alt: "8"},
+        {src: "/9.jpg", alt: "9"}
+    ]
+    }
+    return (
+     
+    )
+
+
+export default Gallery;
