687ebc739e428a52d1e72881eb91722f1eef147b
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-03 18:59:42 +0500

    update cart

--- src/assets/components/Cart.jsx
+++ src/assets/components/Cart.jsx
@@ -3,16 +3,16 @@ import { CartContext } from "./Context/Carts";
 
 function Cart() {
     const cart = useContext(CartContext);
-   
+   const total = cart.items.reduce((a, b) => a + b.price, 0)
     
     return (
             <div className="text-center"><h1 className="text-2xl p-2 font-medium">Cart</h1>
         <div>
-            <div className="text-center">
+            <div className="text-center list-none">
         {
             cart && cart.items.map((item) => <li>{item.name}- ${item.price}</li>)
         }</div>
-            <div><h5 className="font-bold p-2 ">Total Bill: $</h5></div>
+            <div><h5 className="font-bold p-2 ">Total Bill: ${total}</h5></div>
             </div>
         </div>
     )
